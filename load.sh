620f42e8c607a90fbd1a2f8328f7135efae5cb59
Jeffrey T. Peckham <abic@ophymx.com>
2014-05-07 19:59:38 -0700

    Switch from submodule to subtree

--- load.sh
+++ load.sh
@@ -1,4 +1,1 @@
-export LD_LIBRARY_PATH="`pwd`/install/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="`pwd`/install/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PATH="`pwd`/install/bin:$PATH"
-export GI_TYPELIB_PATH="`pwd`/install/lib/girepository-1.0:$GI_TYPELIB_PATH"
+.valaproj/examples/load.sh
